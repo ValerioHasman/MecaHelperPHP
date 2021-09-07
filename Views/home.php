9d60b7f90b4d9879570061966a74d50a227df32e
Valerio <valeriopro22@gmail.com>
2021-09-07 11:35:27 -0300

    Meca

--- Views/home.php
+++ Views/home.php
@@ -1,1 +1,17 @@
-
+<div id="carouselExampleSlidesOnly" class="carousel slide imagemRolando" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="midias/black-and-white-car-vehicle-vintage.jpg" class="d-block w-100"
+                alt="pexels oficinas mecânicas">
+        </div>
+        <div class="carousel-item">
+            <img src="midias/pexels-photo-2244746.jpeg" class="d-block w-100" alt="pexels oficinas mecânicas">
+        </div>
+        <div class="carousel-item">
+            <img src="midias/pexels-photo-4482014.jpeg" class="d-block w-100" alt="pexels oficinas mecânicas">
+        </div>
+        <div class="carousel-item">
+            <img src="midias/pexels-photo-4489708.jpeg" class="d-block w-100" alt="pexels oficinas mecânicas">
+        </div>
+    </div>
+</div>
--- Views/template.php
+++ Views/template.php
@@ -55,26 +55,6 @@
         </div>
         </div>
     </nav>
-    <div id="carouselExampleSlidesOnly" class="carousel slide imagemRolando" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="https://images.pexels.com/photos/474/black-and-white-car-vehicle-vintage.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                    class="d-block w-100" alt="pexels oficinas mecânicas">
-            </div>
-            <div class="carousel-item">
-                <img src="https://images.pexels.com/photos/2244746/pexels-photo-2244746.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                    class="d-block w-100" alt="pexels oficinas mecânicas">
-            </div>
-            <div class="carousel-item">
-                <img src="https://images.pexels.com/photos/4482014/pexels-photo-4482014.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                    class="d-block w-100" alt="pexels oficinas mecânicas">
-            </div>
-            <div class="carousel-item">
-                <img src="https://images.pexels.com/photos/4489708/pexels-photo-4489708.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                    class="d-block w-100" alt="pexels oficinas mecânicas">
-            </div>
-        </div>
-    </div>
 
 
 
