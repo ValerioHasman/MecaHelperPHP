deabe7a5679f88a21ba1cdd255079d0bd754cd7d
Valerio <valeriopro22@gmail.com>
2021-09-14 12:10:19 -0300

    Meca

--- Views/sobre.php
+++ Views/sobre.php
@@ -1,7 +1,7 @@
 <div id="conteudo">
     <h1>Sobre</h1>
     <p>
-        Seja bem vindo ao MecaHelper, Mecânico! Seu assistente online ...Gratuito! Com o Mecahelper, você não só
+        Seja bem 👍👍👍 vindo ao MecaHelper, Mecânico! Seu assistente online ...Gratuito! Com o Mecahelper, você não só
         escreverá facilmente, mas também poderá cadastrar melhor seus clientes e peças, além de acompanhar gráficos que
         te
         dirão como anda o seu negócio.
